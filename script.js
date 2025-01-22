82c64f64b4a57848b704eacb9c13cdd8ae57164e
OkimaDEmba <okimashikiji@gmail.com>
2025-01-22 18:17:22 +0100

    Je fais des test sur le bouton pour qu'il change une image mais ça marche pas.

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let toggle = true;
 const img = document.getElementById("dinapic");
-cont toggleP = document.getElementById("togglepic");
-img.addEventListener("click", function(){
-  toggle = !toggle;
-  if (toggle === true){
-    img.src = 'images\\dina.jpg';
-  }
-  else {
-    img.src = 'images\\curseddina.jpg';
+let checkbox = document.getElementById("test");
+
+
+checkbox.addEventListener("change", function(){
+  if (this.checked){
+    document.getElementById("dinapic").src = 'images\\curseddina.jpg';
   }
 })
